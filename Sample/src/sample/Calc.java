136f1bdd06ad54e4423d1d1163b95c682f08d624
dir-FY24moriryoto <ryoto.mori.p942@dirbato.co.jp>
2024-04-08 09:55:51 +0900

    lesson2 計算

---
+++ Sample/src/sample/Calc.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class Calc{
+    public static void main (String) []args){
+        var num1 = 100;
+        var num2 = 10
+                
+                //足し算
+                var result1 = num1 + num2;
+        System.out.println("num1 + num 2 = "+result1);
+        
+        //引き算
+        var result1 = num1 + num2;
+        System.out.println("num1 - num 2 = "+result2);
+        
+        //掛け算
+        var result1 = num1 + num2;
+        System.out.println("num1 * num 2 = "+result3);
+        
+        //割り算
+        var result1 = num1 + num2;
+        System.out.println("num1 / num 2 = "+result4);
+        
+        //剰余
+        var result1 = num1 + num2;
+        System.out.println("num1 % num 2 = "+result5);
+    }
+}
--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -7,8 +7,8 @@ public class Variable {
         System.out.println(number1);
 
         
-        String str1;
-        str1 = "abc";
+        String str1; str1 = "abc";
+       
         System.out.println(str1);
     }
 
